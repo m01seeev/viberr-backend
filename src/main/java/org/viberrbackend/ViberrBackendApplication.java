94273292af68f0201d591b377740aa7cf3b124a6
Artem <moiseevartem-7@yandex.ru>
2024-12-30 17:57:06 +0400

    create project

---
+++ src/main/java/org/viberrbackend/ViberrBackendApplication.java
@@ -0,0 +1,13 @@
+package org.viberrbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViberrBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViberrBackendApplication.class, args);
+    }
+
+}
