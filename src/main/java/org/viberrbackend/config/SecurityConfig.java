d7f46932bf730e61e928f0639423f9de65d70103
Artem <moiseevartem-7@yandex.ru>
2025-01-04 03:15:18 +0400

    feat(config): add security and password encoder configurations
    - Added `PasswordEncoderConfig` with a bean for password encoding.
    - Added `SecurityConfig`:
      - Configures JWT authentication with `JwtAuthenticationFilter`.
      - Enables stateless session management.
      - Permits access to `/auth/**` endpoints while securing others.

---
+++ src/main/java/org/viberrbackend/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package org.viberrbackend.Config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.viberrbackend.Jwt.JwtAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll() // Разрешаем публичные эндпоинты
+                        .anyRequest().authenticated()           // Все остальные требуют авторизации
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Без сессий
+                );
+
+        // Добавляем JWT фильтр перед фильтром авторизации
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+}
